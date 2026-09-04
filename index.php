beab8ba117c75503fd23c508f453704084f0a149
Hbx <haseebbuildx@gmail.com>
2026-09-04 21:08:48 +0500

    fixs(1)

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require dirname(__DIR__) . '/app/bootstrap.php';
-
+// require dirname(__DIR__) . '/app/bootstrap.php';
+require __DIR__ . '/app/bootstrap.php';
 use App\Core\Router;
 use App\Controllers\InstallController;
 use App\Controllers\AuthController;
