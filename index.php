1870503be9d2fb8d8052147fe929c8ef615bcc45
Hbx <haseebbuildx@gmail.com>
2026-09-08 03:07:41 +0500

    setup

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-require dirname(__DIR__) . '/app/bootstrap.php';
-
+// require dirname(__DIR__) . '/app/bootstrap.php';
+require __DIR__ . '/app/bootstrap.php';
 use App\Core\Application;
 
 $app = new Application();
